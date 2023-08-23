7f6b352dec5115785d3210cbe8d2005db06aea19
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-23 12:19:02 -0500

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,8 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
-    return jsonify({"status": "OK"})
+    response = {"status": "OK"}
+    return jsonify(response)
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
